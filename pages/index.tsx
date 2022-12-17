91a49c59b65cc4f651ff446dc8cd90e55a346441
chizidotdev <chizivwokoma@gmail.com>
2022-12-17 21:01:35 +0100

    build: configure prettier, eslint and workspace configs

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,14 @@ import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div className=''>
+    <div className="">
       <Head>
         <title>BraceUp Africa</title>
-        <meta name='description' content='BraceUp Africa Web Application' />
-        <link rel='icon' href='/favicon.ico' />
+        <meta name="description" content="BraceUp Africa Web Application" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className='text-3xl'>BraceUp Africa!!!</div>
+      <div className="text-3xl">BraceUp Africa!!!</div>
     </div>
   );
 }
